7f1fb0b1ab33546b376c798f372f274bc9be6948
ayeV <ayelenvaldez07@gmail.com>
2019-09-28 17:36:36 -0300

    aaaaaaaaaa

---
+++ Clases2C/Servidor/public/persona.js
@@ -0,0 +1,7 @@
+function Persona(nombre,apellido,email,genero)
+{
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.email = email;
+    this.genero =genero;
+}
--- Clases2C/Servidor/public/script.js
+++ Clases2C/Servidor/public/script.js
@@ -1,24 +1,114 @@
 
-window.addEventListener('load',traerPersonas);
+var xhr = new XMLHttpRequest();
+window.addEventListener('load', () => {
+    document.getElementById("btnTraer").addEventListener('click', traerPersonas);
+});
+
+function crearTabla(array) {
+    debugger;
+    var tabla = document.createElement('table');
+    tabla.setAttribute('class','tabla');
+    let cabecera = document.createElement('tr');
+ 
+    for (atributo in array[0]) {
+        let th = document.createElement('th');
+        th.textContent = atributo;
+        cabecera.appendChild(th);
+    }
+    tabla.appendChild(cabecera);
+
+    for (var i in array) {
+        var fila = document.createElement('tr');
+        var obj = array[i];
+        for (var j in obj) {
+            var celda = document.createElement('td');
+            var dato = document.createTextNode(obj[j]);
+            celda.appendChild(dato);
+            fila.appendChild(celda);
+        }
+        tabla.appendChild(fila);
+
+       celda.addEventListener('dbclick',abrirFormulario);
+
+    }
+    return tabla;
+
+}
+
+function abrirFormulario ()
+{
+    var form = document.getElementById('divCargarPersona');
+    form.hidden = false;
+}
+
+function traerPersonas() {
+
+    xhr.onreadystatechange = callback;
+    xhr.open('GET','http://localhost:3000/traerPersonas',true);
+    xhr.send();
 
 
 
-function altaPersona()
- {
-     let persona = formToObject();
+}
 
-    let xhr= new XMLHttpRequest();
-    xhr.onreadystatechange = ()=>{
-       if( xhr.readyState ==4 && xhr.status == 200){
-           console.log(JSON.parse(xhr.responseText));
-       }
+function callback() {
+    let spin = document.getElementById('spinner');
+    let div = document.getElementById('info');
+    if (xhr.readyState == 4) {
+        if (xhr.status == 200) {
+          
+            let lista = JSON.parse(xhr.responseText);
+            div.appendChild(crearTabla(lista));
+            spin.hidden = true;
+        }
+        else
+        {
+            console.log("Error " + xhr.status + "--" + xhr.statusText);    
+        }
+        
+
+    }
+    else
+    {
+      
+      spin.hidden = false;
+    }
+
+}
+
+
+
+function altaPersona(e) {
+    var form = document.getElementById('divCargarPersona');
+    form.hidden = false;
+    e.preventDefault();
+    let persona = traerDatosDelForm();
+
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            //console.log(JSON.parse(xhr.responseText));
+        }
     }
     xhr.open('POST', 'http://localhost:3000/altaPersona', true);
     xhr.setRequestHeader('Content-type', 'application/json');
     xhr.send(JSON.stringify(persona));
- }
+}
 
- function formToObject()
- {
-     
- }
+function traerDatosDelForm() {
+    debugger;
+    let first_name = document.getElementById('txtNombre').value;
+    let last_name = document.getElementById('txtApellido').value;
+    let email = document.getElementById('txtEmail').value;
+    let male = document.getElementById('rdoMale').checked;
+    let female = document.getElementById('rdoFemale').checked;
+    let gender = '';
+    if(male)
+        gender = male.value;
+    else
+        gender = female.value;
+
+    var obj = new Persona(first_name,last_name,email,gender);
+    return obj;
+
+}
